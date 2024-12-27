248b10d07679c509e4fe306ad92855337d320946
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-12-27 20:17:51 +0300

    chore: initial commit

---
+++ src/presentation/astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import compressor from "astro-compressor";
+
+export default defineConfig({
+  site: "https://antkayaiklimlendirme.com",
+  prefetch: true,
+  srcDir: "src/",
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "tr",
+        locales: {
+          tr: "tr",
+          en: "en",
+        },
+      },
+    }),
+    compressor({
+      gzip: false,
+      brotli: true,
+    }),
+  ],
+  experimental: {
+    clientPrerender: true,
+  },
+});
---
+++ src/presentation/src/shared/constants/constants.ts
@@ -0,0 +1,39 @@
+import ogImageSrc from "~/shared/assets/images/image-not-found.svg";
+
+export const SITE = {
+  title: "Lorem Ipsum",
+  tagline: "Dolor Sit Amet",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  description_short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  url: "https://loremipsum.com",
+  author: "John Doe",
+};
+
+export const SEO = {
+  title: SITE.title,
+  description: SITE.description,
+  structuredData: {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    inLanguage: "en-US",
+    "@id": SITE.url,
+    url: SITE.url,
+    name: SITE.title,
+    description: SITE.description,
+    isPartOf: {
+      "@type": "WebSite",
+      url: SITE.url,
+      name: SITE.title,
+      description: SITE.description,
+    },
+  },
+};
+
+export const OG = {
+  locale: "en_US",
+  type: "website",
+  url: SITE.url,
+  title: `${SITE.title}: : Dolor Sit Amet`,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  image: ogImageSrc,
+};
